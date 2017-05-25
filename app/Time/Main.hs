8d6c9a148e701e2e48b8e44a3e93c1384efbb95b
Vladimir Pankov <wowofbob@gmail.com>
2017-05-25 18:15:17 -0400

    Experimenting.

--- app/Time/Main.hs
+++ app/Time/Main.hs
@@ -1,3 +1,26 @@
 module Main where
 
-main = pure ()
+import Data.ByteString.Lazy (ByteString)
+import Network.HTTP.Conduit
+import Text.HTML.TagSoup
+
+
+get :: String -> IO String
+get url = simpleHTTP
+
+-- https://time.is/
+url :: String
+url = "https://www.google.ru/search?q=current+time"
+
+getHTML :: IO ByteString
+getHTML = get url
+
+main :: IO ()
+main = getHTML >>= print
+  --tags <- parseTags <$> getHTML
+  --print tags 
+  {-
+    dropWhile (\ tag ->
+      case tag of
+        TagOpen name _ -> name /= "body"
+        _              -> True) tags-}
