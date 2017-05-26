265d8f9793967ca051ded1a7c511773fad126101
Vladimir Pankov <vladap@fiord.com>
2017-05-26 13:34:12 +0300

    Added time retrieving combinator.

--- app/Time/Main.hs
+++ app/Time/Main.hs
@@ -1,26 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Data.ByteString.Lazy (ByteString)
+import Data.Text.Lazy (Text)
+import Data.Text.Lazy.Encoding
 import Network.HTTP.Conduit
 import Text.HTML.TagSoup
 
 
-get :: String -> IO String
-get url = simpleHTTP
+type URL = String
 
--- https://time.is/
-url :: String
-url = "https://www.google.ru/search?q=current+time"
 
-getHTML :: IO ByteString
-getHTML = get url
+getTime
+  -- Url to html with time.
+  :: URL
+  -- How to decode html.
+  -> (ByteString -> Text)
+  -- How to extract time.
+  -> ([Tag Text] -> Maybe Text)
+  -- Maybe return something.
+  -> IO (Maybe Text)
+getTime url decodeHtml parseTime =
+  parseTime . parseTags . decodeHtml <$> simpleHttp url
+
+parseTime :: [Tag Text] -> Maybe Text
+parseTime tags =
+  case getTimeTag tags of
+    TagText time -> Just time
+    _            -> Nothing
+  where
+
+    -- Is tag a division containing time?
+    isTimeDiv (TagOpen name [(attr, val)]) =
+      name == "div" && attr == "id" && val == "twd"
+    isTimeDiv _ = False
+    
+    -- Extract time tag.
+    getTimeTag =
+      head . tail . dropWhile (not . isTimeDiv)
+
 
 main :: IO ()
-main = getHTML >>= print
-  --tags <- parseTags <$> getHTML
-  --print tags 
-  {-
-    dropWhile (\ tag ->
-      case tag of
-        TagOpen name _ -> name /= "body"
-        _              -> True) tags-}
+main = getTime "https://time.is" decodeUtf8 parseTime >>= print
