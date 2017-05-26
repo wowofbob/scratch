8a542e739635ff089434a2dacc6e0234e8166a9a
Vladimir Pankov <wowofbob@gmail.com>
2017-05-26 13:03:58 -0400

    Experimenting

--- app/PHP/BB/Main.hs
+++ app/PHP/BB/Main.hs
@@ -1,6 +1,20 @@
 module Main where
 
+import Data.Text.Lazy
+import Data.Text.Lazy.Encoding
+
+import qualified Data.ByteString.Lazy as B
+
+import Text.HTML.TagSoup
+import Text.HTML.TagSoup.Tree
+
+import HTML
 import PHP.BB
 
+
+
+readExampleHtml :: IO B.ByteString
+readExampleHtml = B.readFile "page-example.html"
+
 main :: IO ()
-main = pure ()
+main = readExampleHtml >>= print
--- src/PHP/BB.hs
+++ src/PHP/BB.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
 module PHP.BB where
 
 import Data.Text.Lazy
 import Data.Text.Lazy.Encoding
 import Data.Word
 import Text.HTML.TagSoup
+import Text.HTML.TagSoup.Tree
+import Text.ParserCombinators.Parsec
 
 import qualified Data.Map.Lazy as M
 
@@ -27,3 +30,11 @@ data ForumPage
   , pageNumber :: Word
   , pageMap    :: M.Map Word URL
   }
+  
+
+isTopic :: TagTree Text -> Bool
+isTopic (TagBranch "li" [("class", "row bg1")] _) = True
+isTopic (TagBranch "li" [("class", "row bg2")] _) = True
+isTopic _                                         = False
+
+
